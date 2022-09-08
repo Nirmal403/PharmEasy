0c481aa3cb820f1e1cb5a0f017b8521aefd0ec4f
Nirmal403 <nirmalcool414@gmail.com>
2022-09-08 20:08:41 +0530

    order button

--- pages/pscript/payment.js
+++ pages/pscript/payment.js
@@ -1,3 +1,9 @@
-document.getElementById("linkwallet").addEventListener("click",function(){
-    window.location.href = "succesfull.html"
+document.getElementsByClassName("wallettext").addEventListener("click",function(){
+    alert("Order is succesfull");
+    window.location.href = "../index.html"
 })
+
+function order(){
+    alert("Order is succesfull");
+    window.location.href = "../index.html";
+}
